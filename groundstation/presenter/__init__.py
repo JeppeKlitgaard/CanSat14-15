a52fe6a4b8277289dbf277d98d4076f2d4b8a551
Jeppe Klitgaard <jeppe@dapj.dk>
2015-06-08 13:24:04 +0200

    Reworked the presenter module structure

--- groundstation/presenter/__init__.py
+++ groundstation/presenter/__init__.py
@@ -5,12 +5,4 @@ collected data to users connected via a webbrowser.
 
 __version__ = (0, 0, 1)
 
-from flask import Flask
-from flask_bootstrap import Bootstrap
-
-app = Flask(__name__)
-app.config["BOOTSTRAP_SERVE_LOCAL"] = True
-
-Bootstrap(app)
-
-from . import views  # noqa unused-import
+from .app import app
---
+++ groundstation/presenter/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config["BOOTSTRAP_SERVE_LOCAL"] = True
+
+Bootstrap(app)
+
+from . import views  # noqa unused-import
