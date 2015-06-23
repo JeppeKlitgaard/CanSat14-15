13d045cf9b9f125337c18dd60965946aa7440efd
Jeppe Klitgaard <jeppe@dapj.dk>
2015-06-23 09:23:01 +0200

    Added production mode - still needs work.

--- run_presenter.py
+++ run_presenter.py
@@ -1,8 +1,22 @@
 """
 Runs the Flask-based webserver.
 """
+import sys
 
 if __name__ == '__main__':
-    from presenter import run
+    try:
+        if sys.argv[1] == "prod":
+            from presenter import run_prod
 
-    run()
+            run_prod()
+
+        elif sys.argv[1] == "dev":
+            from presenter import run_dev
+
+            run_dev()
+
+        else:
+            raise IndexError
+
+    except IndexError:
+        sys.exit("Argument must be either 'prod' or 'dev'!")
