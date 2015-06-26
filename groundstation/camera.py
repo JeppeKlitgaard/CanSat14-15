7e6f17ac1da36cadbb8ad9efd2cc6d3e6911ab71
Jeppe Klitgaard <jeppe@dapj.dk>
2015-06-27 00:41:13 +0200

    Include time in output format

--- groundstation/camera.py
+++ groundstation/camera.py
@@ -1,7 +1,8 @@
 import re
 import os
 
-pattern = re.compile(r"(FFD8.+?FFD9)", re.DOTALL | re.MULTILINE)
+pattern = re.compile(r"\nEnd of pic\n\n(\d+)\nStart pic\n(FFD8.+?FFD9)",
+                     re.DOTALL | re.MULTILINE)
 
 
 def read_file(filepath):
@@ -14,28 +15,30 @@ def read_file(filepath):
 
     image_matches = pattern.findall(data_content)
 
-    byte_objs = []
+    objs = []
 
-    for image in image_matches:
+    for (time, image) in image_matches:
         image = image.replace("\n", "")  # remove newlines
-        byte_objs.append(bytes.fromhex(image))
+        objs.append([time, bytes.fromhex(image)])
 
-    return byte_objs
+    return objs
 
 
 def read_dir(filepath):
     files = os.listdir(filepath)
 
-    byte_objs = []
+    objs = []
 
     for f in files:
         try:
             f = os.path.join(filepath, f)
-            byte_objs.append(read_file(f))
+            objs.append(read_file(f))
         except (UnicodeDecodeError, ValueError):
             pass
 
-    for byte_obj_m, i in zip(byte_objs, range(len(byte_objs))):
-        for byte_obj_i, ii in zip(byte_obj_m, range(len(byte_obj_m))):
-            with open("img_batch{}_image{}.jpg".format(str(i), str(ii)), "wb") as f_out:
-                f_out.write(byte_obj_i)
+    for (byte_obj_m, i) in zip(objs, range(len(objs))):
+
+        for (byte_obj_i, ii) in zip(byte_obj_m, range(len(byte_obj_m))):
+
+            with open("img_batch{}_image{}___time{}.jpg".format(str(i), str(ii), str(byte_obj_i[0])), "wb") as f_out:
+                f_out.write(byte_obj_i[1])
